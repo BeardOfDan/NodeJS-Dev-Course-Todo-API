bfbd4cc6db84684166db86bc4e12015fbad22df2
Dan <DanielWRoberts@outlook.com>
2017-08-04 14:38:27 -0700

    Added a user model
    The user model is very basic, and it will have further changes made.
    Currently, it only has an email field, with string validation.
    It still needs to have validation that the string is a valid email
    address.

--- server/server.js
+++ server/server.js
@@ -11,29 +11,59 @@ mongoose.connect("mongodb://localhost:27017/TodoApp", {
 
 let Todo = mongoose.model("Todo", {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 5,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
     // Unix time stamp
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-let newTodo = new Todo({
-  text: "Cook dinner",
-  completed: true,
-  completedAt: 1234
+// let newTodo = new Todo({
+//   text: "Do some thing"
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log("Saved todo", JSON.stringify(doc, undefined, 2));
+// }, (err) => {
+//   console.log("Unable to save todo", err);
+// })
+//   .then(() => {
+//     mongoose.disconnect();
+// });
+
+// User model
+// email - required - trim - type string - min length 5
+
+// create a new user with + without an email to test requirements
+// chekc the database to see it
+
+let User = mongoose.model("User", {
+  email : {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5
+  }
 });
 
-newTodo.save().then((doc) => {
-  console.log("Saved todo", JSON.stringify(doc, undefined, 2));
+let user = new User({
+  email: "a@s.d"
+});
+
+user.save().then((doc) => {
+  console.log("Saved the new user:\n", doc);
 }, (err) => {
-  console.log("Unable to save todo", err);
-})
-  .then(() => {
-    mongoose.disconnect();
+  console.log("Error, was unable to save new user:\n", err);
+}).then(() => {
+  mongoose.disconnect();
 });
 
