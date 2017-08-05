00f3465e34d3a30c87483dff72972159b9ad88af
Dan <DanielWRoberts@outlook.com>
2017-08-04 17:48:19 -0700

    Added test for GET /todos
    Altered the BeforeEach function. Now, after it removes everything
    from the database, it inserts an array of 2 todos (so that the GET
    tests have data to test with). The other tests were updated so that
    the don't throw errors due to this change.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,9 +6,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: "First test todo"
+}, {
+  text: "Second test todo"
+}];
+
 // ensures that the database is empty before every test is run
 beforeEach((done) => {
   Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => {
     done();
   });
 });
@@ -25,14 +33,14 @@ describe("POST /todos", () => {
         expect(res.body.text).toBe(text);
       })
       .then(() => {
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
+        }).catch((err) => {
+          done(err);
         });
-      }).catch((err) => {
-      done(err);
-    });
+      });
   });
 
   it("should not create todo with invalid body data", (done) => {
@@ -42,12 +50,24 @@ describe("POST /todos", () => {
       .expect(400)
       .then(() => {
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
+        }).catch((err) => {
+          done(err);
         });
-      }).catch((e) => {
-        done(e);
-    });
+      });
   });
 
 }); // end of describe "POST /todos"
+
+describe("GET /todos", () => {
+  it("should get all todos", (done) => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+}); // end of describe GET /todos
