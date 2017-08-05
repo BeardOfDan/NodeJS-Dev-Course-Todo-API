59280f4d4aaa19511bc42c0a8d6c12cfb3b7f402
Dan <DanielWRoberts@outlook.com>
2017-08-04 21:53:08 -0700

    Added DELETE /todos/:id route
    The user can now delete a specific todo if they have the id

--- server/server.js
+++ server/server.js
@@ -49,9 +49,25 @@ app.get("/todos/:id", (req, res, next) => {
   }).catch((err) => {
     res.status(400).send();
   });
-
 });
 
+app.delete("/todos/:id", (req, res, next) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(todo);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
