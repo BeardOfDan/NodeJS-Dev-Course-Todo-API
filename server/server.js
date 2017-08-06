d20c56c20ce092b8f49ecab8ea517ccf92feffcf
Dan <DanielWRoberts@outlook.com>
2017-08-06 16:14:51 -0700

    Added GET /users/me and authentication middleware
    Authentication is from a file "server/middleware/authenticate.js".
    Also, a number of changes were made to the User model in user.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require("./middleware/authenticate");
 
 let app = express();
 
@@ -68,6 +69,10 @@ app.get("/users", (req, res, next) => {
     .catch((err) => {
       res.status(400).send(err);
   });
+});
+
+app.get("/users/me", authenticate, (req, res, next) => {
+  res.send(req.user);
 });
 
 app.get("/todos/:id", (req, res, next) => {
